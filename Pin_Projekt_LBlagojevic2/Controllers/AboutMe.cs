f1173ba3e1e186aa41c02521ec75567d83f0f58b
Luka Blagojević <lublagoj@vsite.hr>
2024-09-09 12:39:53 +0200

    about me

---
+++ Pin_Projekt_LBlagojevic2/Controllers/AboutMe.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Pin_Projekt_LBlagojevic2.Controllers
+{
+    public class AboutMe : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
